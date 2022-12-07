ca6c325b67fae482f449abe60bcb596738d4f10f
Linzing Andrei <107073731+914-Linzing-Andrei@users.noreply.github.com>
2022-12-07 23:22:17 +0200

    Create ex9.asm

---
+++ exercises/ex9/ex9.asm
@@ -0,0 +1,79 @@
+bits 32 ; assembling for the 32 bits architecture
+
+; declare the EntryPoint (a label defining the very first instruction of the program)
+global start        
+
+; declare external functions needed by our program
+extern exit, fopen, fclose, fprintf, fread             
+import exit msvcrt.dll
+import fopen msvcrt.dll
+import fclose msvcrt.dll
+import fprintf msvcrt.dll    
+import fread msvcrt.dll
+
+; our data is declared here (the variables needed by our program)
+segment data use32 class=data
+    text db "asdg142412 124 124 214   12412asaasd3mkate"
+    len equ $-text
+    rez times len db 0
+    
+    fileName db "out.txt",0
+    accessMode db "w",0
+    fileDescriptor dd -1
+    
+    format db "%s",0
+    index db 0
+
+; our code starts here
+segment code use32 class=code
+    start:
+        int3
+        
+        mov esi,text
+        mov edi,rez
+        mov ecx,len
+        
+        cld
+        
+        repeta:
+            lodsb
+            inc byte[index]
+            test byte[index],0x01
+            jnz odd_pos
+            
+            
+            stosb
+            
+            jmp skip
+            
+            odd_pos:
+                mov al,'X'
+                stosb
+        
+        skip: loop repeta
+        
+        print:
+            push dword accessMode
+            push dword fileName
+            call [fopen]
+            add esp, 4 * 2
+            
+            mov [fileDescriptor],eax
+            
+            cmp eax,0
+            je end_
+            
+            push dword [rez]
+            push dword format
+            push dword [fileDescriptor]
+            call [fprintf]
+            add esp, 4 * 3
+            
+            push dword [fileDescriptor]
+            call [fclose]
+            add esp,4
+            
+            end_:
+        
+        push    dword 0      ; push the parameter for exit onto the stack
+        call    [exit]       ; call exit to terminate the program
