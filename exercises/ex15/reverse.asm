f185c4181919f351f93807f152cf44369c8604c3
Linzing Andrei <107073731+914-Linzing-Andrei@users.noreply.github.com>
2023-01-07 19:03:57 +0200

    Update reverse.asm

--- exercises/ex15/reverse.asm
+++ exercises/ex15/reverse.asm
@@ -1,21 +1,27 @@
 global reverse
 
 segment data use32 class = data
+    numar db 2
 
 segment code use32 class = code
     reverse:
-        mov ecx, 4
-        add edi, 7
-        mov esi, edi
-        sub edi, 7
         xor eax,eax
+        xor ebx,ebx
+        mov edi,esi
+        sub edi,1
+        sub esi,edx
+        mov eax, edx
+        xor edx,edx
+        div byte [numar]
+        mov ecx, eax
+        
         repeta:
-            mov al,[esi]
-            mov ah,[edi]
-            xchg al,ah
-            mov [esi],al
-            mov [edi],ah
-            inc edi
-            dec esi
+            mov bl,[esi]
+            mov bh,[edi]
+            xchg bl,bh
+            mov [esi],bl
+            mov [edi],bh
+            inc esi
+            dec edi
         loop repeta
         ret
